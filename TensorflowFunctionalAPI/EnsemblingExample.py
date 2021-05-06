9c42926658383d2b25ea4756a07bdad9e00e518b
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-05-06 17:49:51 +0530

    Feat: ensembling example

---
+++ TensorflowFunctionalAPI/EnsemblingExample.py
@@ -0,0 +1,37 @@
+# This code is based on : https://www.tensorflow.org/guide/keras/functional#all_models_are_callable_just_like_layers
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import cv2
+
+"""
+In the code below, get_model function returns a convolution model.
+model1 , model2 , model3 creates 3 instances of the same model architecture.
+Using the layers.average function the ensemble_model ensemble the set of models into a single model that averages their predictions
+"""
+
+def get_model():
+    inputs = keras.Input(shape=(28, 28, 1), name="img")
+    x = layers.Conv2D(16, 3, activation="relu")(inputs)
+    x = layers.Conv2D(32, 3, activation="relu")(x)
+    x = layers.MaxPooling2D(3)(x)
+    x = layers.Conv2D(32, 3, activation="relu")(x)
+    x = layers.Conv2D(16, 3, activation="relu")(x)
+    outputs = layers.GlobalMaxPooling2D()(x)
+    return keras.Model(inputs, outputs)
+
+
+convModel1 = get_model()
+convModel2 = get_model()
+convModel3 = get_model()
+
+inputs = keras.Input(shape=(28, 28, 1), name="img")
+y1 = convModel1(inputs)
+y2 = convModel2(inputs)
+y3 = convModel3(inputs)
+
+outputs = layers.average([y1, y2, y3])
+ensemble_model = keras.Model(inputs=inputs, outputs=outputs)
+
---
+++ TensorflowFunctionalAPI/SubModelsExample.py
@@ -0,0 +1,39 @@
+# This code is based on : https://www.tensorflow.org/guide/keras/functional#all_models_are_callable_just_like_layers
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import cv2
+
+# Defining the funtion to retun a model
+
+
+# Creating Encoder model
+encoder = keras.Model(encoder_input, encoder_output, name="encoder")
+encoder.summary()
+
+# Defining the Decoder
+decoder_input = keras.Input(shape=(16,), name="encoded_img")
+x = layers.Reshape((4, 4, 1))(decoder_input)
+x = layers.Conv2DTranspose(16, 3, activation="relu")(x)
+x = layers.Conv2DTranspose(32, 3, activation="relu")(x)
+x = layers.UpSampling2D(3)(x)
+x = layers.Conv2DTranspose(16, 3, activation="relu")(x)
+decoder_output = layers.Conv2DTranspose(1, 3, activation="relu")(x)
+
+# Creating Decoder Model
+decoder = keras.Model(decoder_input, decoder_output, name="decoder")
+decoder.summary()
+
+"""
+In the code below, autoencoder_input is the input layer which is passed to the
+encoder model which outputs a encoded_img.
+The encoded_img is passed to the decoder which outputs the decoded_img which is also 
+the autoencoders output
+"""
+autoencoder_input = keras.Input(shape=(28, 28, 1), name="img")
+encoded_img = encoder(autoencoder_input)
+decoded_img = decoder(encoded_img)
+autoencoder = keras.Model(autoencoder_input, decoded_img, name="autoencoder")
+autoencoder.summary()
