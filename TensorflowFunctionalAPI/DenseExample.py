850478d253076279d41dc5b5c0602840948dba7b
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-05-07 23:46:14 +0530

    Refactor : Dense Layer Names

--- TensorflowFunctionalAPI/DenseExample.py
+++ TensorflowFunctionalAPI/DenseExample.py
@@ -9,12 +9,12 @@ import cv2
 inputs = keras.Input(shape=(32, 32, 3) , name="img")
 
 # Define the First Hidden Layers:
-x = layers.Dense(255, activation="relu")(inputs) 
+x = layers.Dense(255, activation="relu" , name="Dense1")(inputs) 
 # Notice how the last layer is passed as a parameter to the first layer
 
 # Adding the rest of the Hidden Layers:
-x = layers.Dense(32, activation="relu")(x)
-x = layers.Dense(16, activation="relu")(x)
+x = layers.Dense(32, activation="relu" , name="Dense2")(x)
+x = layers.Dense(16, activation="relu" , name="Dense3")(x)
 
 # Adding the Last Layer:
 outputs = layers.Dense(10)(x)
