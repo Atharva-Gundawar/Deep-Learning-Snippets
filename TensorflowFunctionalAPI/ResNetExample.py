df4313fbd25694879680600ca129777a707e968a
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-05-07 12:06:54 +0530

    Refactor : ResNet

--- TensorflowFunctionalAPI/ResNetExample.py
+++ TensorflowFunctionalAPI/ResNetExample.py
@@ -62,3 +62,10 @@ model.compile(
     loss='sparse_categorical_crossentropy',
     metrics=['accuracy']
 )
+
+# Getting the Model summary:
+model.summary()
+
+# Display the Model in a Block Diagram  
+keras.utils.plot_model(model, "model_image.png") # Include 'show_shapes=True' as a parameter to display th shapes of the respective layers.
+cv2.imshow("model_image.png")
