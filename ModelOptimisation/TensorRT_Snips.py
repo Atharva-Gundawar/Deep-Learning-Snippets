ad41db41c7472df01ae9a976475021bddbd126b0
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-05-09 23:58:52 +0530

    Fix : Indent

--- ModelOptimisation/TensorRT_Snips.py
+++ ModelOptimisation/TensorRT_Snips.py
@@ -32,20 +32,20 @@ model = InceptionV3(weights='imagenet')
 
 # Predction Fucntion
 def show_predictions(model,num_images=4):
-  for i in range(num_images):
-    img_path = './data/img%d.JPG'%i
-    img = image.load_img(img_path, target_size=(299, 299))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
+    for i in range(num_images):
+        img_path = './data/img%d.JPG'%i
+        img = image.load_img(img_path, target_size=(299, 299))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
 
-    preds = model.predict(x)
-    print('{} - Predicted: {}'.format(img_path, decode_predictions(preds, top=3)[0]))
+        preds = model.predict(x)
+        print('{} - Predicted: {}'.format(img_path, decode_predictions(preds, top=3)[0]))
 
-    plt.subplot(num_images//2,2,i+1)
-    plt.imshow(img)
-    plt.axis('off')
-    plt.title(decode_predictions(preds, top=3)[0][0][1])
+        plt.subplot(num_images//2,2,i+1)
+        plt.imshow(img)
+        plt.axis('off')
+        plt.title(decode_predictions(preds, top=3)[0][0][1])
 
 show_predictions(model)
 
@@ -54,14 +54,14 @@ tf.saved_model.save(model, 'inceptionv3_saved_model')
 
 # Returns Batchs for Input  
 def batch_input(batch_size=8):
-  batched_input = np.zeros((batch_size,299,299,3),dtype=np.float32)
-  for i in range(batch_size):
-    img_path = './data/img%d.JPG' % (i % 4)
-    img = image.load_img(img_path, target_size=(299,299))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    batched_input[i, : ] = x
+    batched_input = np.zeros((batch_size,299,299,3),dtype=np.float32)
+    for i in range(batch_size):
+        img_path = './data/img%d.JPG' % (i % 4)
+        img = image.load_img(img_path, target_size=(299,299))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        batched_input[i, : ] = x
 
     return tf.constant(batched_input)
 
@@ -69,8 +69,8 @@ batched_input = batch_input(batch_size=32)
 
 # Load TensorFlow SavedModel
 def load_tf_saved_model(input_saved_model_dir):
-  print(f'Loading from {input_saved_model_dir}')
-  return tf.saved_model.load(input_saved_model_dir, tags = [tag_constants.SERVING])
+    print(f'Loading from {input_saved_model_dir}')
+    return tf.saved_model.load(input_saved_model_dir, tags = [tag_constants.SERVING])
 
 # Here we load a previously saved InceptionV3 model.
 saved_model = load_tf_saved_model('inceptionv3_saved_model')
@@ -80,29 +80,31 @@ print(infer.structured_outputs)
 # Function to get Baseline for Prediction Throughput and Accuracy 
 def predict_and_benchmark_throughput(batched_input, infer, N_warmup_run=50, N_run=1000):
 
-  elapsed_time = []
-  all_preds = []
-  batch_size = batched_input.shape[0]
+    elapsed_time = []
+    all_preds = []
+    batch_size = batched_input.shape[0]
 
-  for i in range(N_warmup_run):
-    labeling = infer(batched_input)
-    preds = labeling['predictions'].numpy()
+    for i in range(N_warmup_run):
+        labeling = infer(batched_input)
+        preds = labeling['predictions'].numpy()
 
-  for i in range(N_run):
-    start_time = time.time()
+    for i in range(N_run):
+        start_time = time.time()
 
-    labeling = infer(batched_input)
+        labeling = infer(batched_input)
 
-    preds = labeling['predictions'].numpy()
+        preds = labeling['predictions'].numpy()
 
-    end_time = time.time()
+        end_time = time.time()
 
-    elapsed_time = np.append(elapsed_time, end_time - start_time)
-    
-    all_preds.append(preds)
+        elapsed_time = np.append(elapsed_time, end_time - start_time)
 
-    if i % 50 == 0:
-      print('Steps {}-{} average: {:4.1f}ms'.format(i, i+50, (elapsed_time[-50:].mean()) * 1000))
+        all_preds.append(preds)
 
-  print('Throughput: {:.0f} images/s'.format(N_run * batch_size / elapsed_time.sum()))
-  return all_preds
+        if i % 50 == 0:
+            print('Steps {}-{} average: {:4.1f}ms'.format(i, i+50, (elapsed_time[-50:].mean()) * 1000))
+
+    print('Throughput: {:.0f} images/s'.format(N_run * batch_size / elapsed_time.sum()))
+    return all_preds
+
+all_preds = predict_and_benchmark_throughput(batched_input,infer)
